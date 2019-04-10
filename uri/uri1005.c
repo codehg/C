4d4a4afe650249add59245fbf9474d3fc0e346c0
Hugo Aragao <hugoaraliveira@gmail.com>
2019-04-10 18:06:55 -0300

    Codigo corrigido

--- uri/uri1005.c
+++ uri/uri1005.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    double a, b, media, tmp; //tmp = temporaria usada como uma variavel auxiliar
+    double a, b, media; 
 
     scanf("%lf %lf", &a, &b);
     
-    tmp = (a + b);
-    media = tmp / 2;
+    a = a * 3.5;
+    b = b * 7.5;
+    media = (a + b) / 11;
 
     printf("MEDIA = %.5lf\n", media);      
 
